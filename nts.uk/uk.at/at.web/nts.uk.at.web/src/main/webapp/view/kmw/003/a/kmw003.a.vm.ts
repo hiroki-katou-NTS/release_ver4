9197400a6886912af1bca51f7ebcc791e645d750
NTS\tin_dh <tin_dh@nittsusystem.co.jp>
2019-04-06 13:15:36 +0900

    cherry pick 6aaa5d41314a9188f4589cae7002dde366fdc197 and resolve conflict

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kmw/003/a/kmw003.a.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kmw/003/a/kmw003.a.vm.ts
@@ -686,17 +686,20 @@ module nts.uk.at.view.kmw003.a.viewmodel {
                     //                        $("#dpGrid").mGrid("updateCell", data.rowId, data.columnKey, data.value, true);
                     //                    });
                     nts.uk.ui.dialog.info({ messageId: 'Msg_15' });
-                    nts.uk.ui.block.clear();
-                    //                    if (self.initMode() != ScreenMode.APPROVAL) {
-                    self.loadRowScreen();
-                    //self.showButton(new AuthorityDetailModel(self.dataAll().authorityDto, self.dataAll().actualTimeState, self.initMode(), self.dataAll().formatPerformance.settingUnitType));
-                    //                           self.updateDate(self.yearMonth());  
-                    //                    }
+					
+//                    if (self.initMode() != ScreenMode.APPROVAL) {
+					self.loadRowScreen().done(() => {
+						nts.uk.ui.block.clear();
+					});
+                        //self.showButton(new AuthorityDetailModel(self.dataAll().authorityDto, self.dataAll().actualTimeState, self.initMode(), self.dataAll().formatPerformance.settingUnitType));
+                        //                           self.updateDate(self.yearMonth());  
+//                    }
                 }).fail(function(res: any) {
                     if(res.optimisticLock === true){
                         nts.uk.ui.dialog.error({ messageId: 'Msg_1528' }).then(() => {
-                            nts.uk.ui.block.clear();
-                            self.loadRowScreen();
+                            self.loadRowScreen().done(() => {
+                                nts.uk.ui.block.clear();
+                            });
                         });
                     } else {
                         nts.uk.ui.dialog.error({ messageId: res.messageId, messageParams: res.parameterIds }).then(function() { nts.uk.ui.block.clear(); });    
