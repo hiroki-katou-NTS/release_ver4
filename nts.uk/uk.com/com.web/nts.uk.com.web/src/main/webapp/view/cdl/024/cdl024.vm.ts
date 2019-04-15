fa9e1691048463665ed7bf793a810d86321bbdc8
Pham Van dan <dan_pv@nittsusystem-vn.com>
2019-04-15 14:54:22 +0900

    fix bug 107267. use ntsGridList( jquery version)

--- nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cdl/024/cdl024.vm.ts
+++ nts.uk/uk.com/com.web/nts.uk.com.web/src/main/webapp/view/cdl/024/cdl024.vm.ts
@@ -1,15 +1,15 @@
 module nts.uk.at.view.cdl024.viewmodel {
     export class ScreenModel {
-        columns: KnockoutObservableArray<any>;
-        items: KnockoutObservableArray<IItemModel> = ko.observableArray([]);
-        currentCodeList: KnockoutObservableArray<string> = ko.observableArray([]);
+        columns: Array<Object>;
+        items: Array<IItemModel>;
+        currentCodeList: Array<String>;
 
         constructor() {
             let self = this;
-            this.columns = ko.observableArray([
+            this.columns = [
                 { headerText: 'コード', prop: 'code', width: 100 },
-                { headerText: '名称', prop: 'name', width: 230, formatter: _.escape }
-            ]); 
+                { headerText: '名称', prop: 'name', width: 258, formatter: _.escape }
+            ]; 
         }
 
         closeDialog() {
@@ -17,22 +17,22 @@ module nts.uk.at.view.cdl024.viewmodel {
         }
 
         sendAttribute() {
-            nts.uk.ui.windows.setShared("currentCodeList", this.currentCodeList());
+            let selectedItems = $("#multi-list").ntsGridList("getSelected");
+            this.currentCodeList = _.map(selectedItems, 'id');
+            nts.uk.ui.windows.setShared("currentCodeList", this.currentCodeList);
             nts.uk.ui.windows.close();
         }
 
         startPage(): JQueryPromise<any> {
             let self = this,
-                items = self.items,
                 dfd = $.Deferred();
 
-            items.removeAll();
             service.getAll().done((data: Array<IItemModel>) => {
                 data = _.orderBy(data, ["code"], ['asc']);
-                items(data);
+                self.items = data;
                 let parameter: InputParam = nts.uk.ui.windows.getShared("CDL024");
                 if (parameter != null && parameter.codeList != null) {
-                    self.currentCodeList(parameter.codeList);
+                    self.currentCodeList = parameter.codeList;
                 }
                 dfd.resolve();
             });
