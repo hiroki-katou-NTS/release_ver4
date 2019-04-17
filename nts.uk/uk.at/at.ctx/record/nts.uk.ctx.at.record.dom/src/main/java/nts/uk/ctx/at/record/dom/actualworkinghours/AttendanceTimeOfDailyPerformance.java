d563194018ba52b045d04098a267a8927647c428
NTS\keisuke_hoshina <keisuke_hoshina@nittsusystem.co.jp>
2019-04-17 10:24:02 +0900

    Fixed bug 大塚乖離No10 休出では求めない #107229

--- nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/actualworkinghours/AttendanceTimeOfDailyPerformance.java
+++ nts.uk/uk.at/at.ctx/record/nts.uk.ctx.at.record.dom/src/main/java/nts/uk/ctx/at/record/dom/actualworkinghours/AttendanceTimeOfDailyPerformance.java
@@ -377,7 +377,8 @@ public class AttendanceTimeOfDailyPerformance extends AggregateRoot {
 					recordReGetClass.getFixRestTimeSetting(),
 					calcResultIntegrationOfDaily.getAttendanceTimeOfDailyPerformance().get()
 							.getActualWorkingTimeOfDaily().getTotalWorkingTime(),
-							recordReGetClass.getWorkTimeSetting());
+							recordReGetClass.getWorkTimeSetting(),
+							recordReGetClass.getWorkType());
 
 			val reCreateActual = ActualWorkingTimeOfDaily.of(
 					calcResultIntegrationOfDaily.getAttendanceTimeOfDailyPerformance().get()
