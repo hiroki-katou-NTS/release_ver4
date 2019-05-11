97f3910614560309192cd730e62c0e9b8fb24f69
NSVN-TrieuKhacTu <tu_tk@nittsusystem-vn.com>
2019-05-11 10:42:59 +0700

    fixbug kal010 :#107686

--- nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.dom/src/main/java/nts/uk/ctx/at/function/dom/alarm/alarmlist/monthly/MonthlyAggregateProcessService.java
+++ nts.uk/uk.at/at.ctx/function/nts.uk.ctx.at.function.dom/src/main/java/nts/uk/ctx/at/function/dom/alarm/alarmlist/monthly/MonthlyAggregateProcessService.java
@@ -743,11 +743,11 @@ public class MonthlyAggregateProcessService {
 			List<AttendanceItemName> listAttdNameSub =  listAttdNameAdds.stream().filter(c -> erAlAtdItemConAdapterDto.getCountableSubAtdItems().contains(c.getAttendanceItemId())).collect(Collectors.toList());
 			nameErrorAlarm = getNameErrorAlarm(listAttdNameSub,1,nameErrorAlarm);//1 sub atd item
 			
-			String nameItem = "";
+			String nameItem = extra.getNameAlarmExtraCon();
 			String alarmDescription = "";
 			switch(extra.getTypeCheckItem()) {
 			case 4 ://時間
-				nameItem = TextResource.localize("KAL010_47");
+				//nameItem = TextResource.localize("KAL010_47");
 				String startValueTime = this.timeToString(startValue.intValue());
 				String endValueTime = "";
 				if(compare<=5) {
@@ -774,7 +774,7 @@ public class MonthlyAggregateProcessService {
 				checkedValue = this.timeToString(Double.valueOf(checkedValue).intValue());
 				break;
 				case 5 ://日数
-					nameItem = TextResource.localize("KAL010_113");
+					//nameItem = TextResource.localize("KAL010_113");
 					String startValueDays = String.valueOf(startValue.intValue());
 					String endValueDays = "";
 					if(compare<=5) {
@@ -800,7 +800,7 @@ public class MonthlyAggregateProcessService {
 					}
 					break;
 				case 6 ://回数
-					nameItem = TextResource.localize("KAL010_50");
+					//nameItem = TextResource.localize("KAL010_50");
 					String startValueTimes = String.valueOf(startValue.intValue());
 					String endValueTimes = "";
 					if(compare<=5) {
@@ -826,7 +826,7 @@ public class MonthlyAggregateProcessService {
 					}
 					break;
 				case 7 ://金額 money
-					nameItem = TextResource.localize("KAL010_51");
+					//nameItem = TextResource.localize("KAL010_51");
 					String startValueMoney = String.valueOf(startValue.intValue());
 					String endValueMoney = "";
 					if(compare<=5) {
@@ -909,9 +909,9 @@ public class MonthlyAggregateProcessService {
 					employee.getId(),
 					this.toYMString(yearMonth),
 					KAL010_100,
-					TextResource.localize("KAL010_60"),
+					extra.getNameAlarmExtraCon(),
 					alarmDescriptionValue,	
-					extra.getDisplayMessage(),null);
+					extra.getDisplayMessage(),TextResource.localize("KAL010_998"));
 			listValueExtractAlarm.add(resultMonthlyValue);
 		}
 
