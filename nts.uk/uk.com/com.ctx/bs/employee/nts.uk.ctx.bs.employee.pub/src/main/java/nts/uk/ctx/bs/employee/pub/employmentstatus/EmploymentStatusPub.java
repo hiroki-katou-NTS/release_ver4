ca86e36a9ca407803f07693dbb253cd25b0dd01d
NSVN-TrieuKhacTu <tu_tk@nittsusystem-vn.com>
2019-06-12 16:56:48 +0700

    resolve conflict SyEmployeePubImp.java

--- nts.uk/uk.com/com.ctx/bs/employee/nts.uk.ctx.bs.employee.pub/src/main/java/nts/uk/ctx/bs/employee/pub/employmentstatus/EmploymentStatusPub.java
+++ nts.uk/uk.com/com.ctx/bs/employee/nts.uk.ctx.bs.employee.pub/src/main/java/nts/uk/ctx/bs/employee/pub/employmentstatus/EmploymentStatusPub.java
@@ -5,7 +5,13 @@ import java.util.List;
 import nts.uk.shr.com.time.calendar.period.DatePeriod;
 
 public interface EmploymentStatusPub {
-	
+	/**
+	 * RequestList 433
+	 * 社員ID（List）と期間から期間中１日ずつの在職状態を取得
+	 * @param employeeIds
+	 * @param period
+	 * @return
+	 */
 	List<EmploymentStatus> findListOfEmployee(List<String> employeeIds, DatePeriod period);
 
 }
