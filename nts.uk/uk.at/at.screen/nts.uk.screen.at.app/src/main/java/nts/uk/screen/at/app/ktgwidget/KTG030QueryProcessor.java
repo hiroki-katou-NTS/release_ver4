421d0448cb4f200844670c0c37b7ace31f411386
NSVC\hieult <hieultph03963@fpt.edu.vn>
2019-07-25 17:01:32 +0700

    fixbug 108418

--- nts.uk/uk.at/at.screen/nts.uk.screen.at.app/src/main/java/nts/uk/screen/at/app/ktgwidget/KTG030QueryProcessor.java
+++ nts.uk/uk.at/at.screen/nts.uk.screen.at.app/src/main/java/nts/uk/screen/at/app/ktgwidget/KTG030QueryProcessor.java
@@ -7,6 +7,9 @@ import java.util.stream.Collectors;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
+import nts.uk.ctx.at.auth.app.find.employmentrole.InitDisplayPeriodSwitchSetFinder;
+import nts.uk.ctx.at.auth.app.find.employmentrole.dto.DateProcessed;
+import nts.uk.ctx.at.auth.app.find.employmentrole.dto.InitDisplayPeriodSwitchSetDto;
 import nts.uk.ctx.at.record.dom.workrecord.actualsituation.checktrackrecord.CheckTargetItemDto;
 import nts.uk.ctx.at.record.dom.workrecord.actualsituation.checktrackrecord.CheckTrackRecord;
 import nts.uk.ctx.at.shared.dom.workrule.closure.ClosureInfo;
@@ -16,30 +19,39 @@ import nts.uk.shr.com.context.AppContexts;
 @Stateless
 public class KTG030QueryProcessor {
 	@Inject
-	private CheckTrackRecord  checkTrackRecord;
+	private CheckTrackRecord checkTrackRecord;
 
 	@Inject
 	private ClosureService closureService;
+	@Inject
+	private InitDisplayPeriodSwitchSetFinder initDisplayPeriodSwitchSetFinder; 
+
+
 	/**
-	 *  月別実績確認すべきデータ有無取得
+	 * 月別実績確認すべきデータ有無取得
+	 * 
 	 * @author tutk
 	 */
 	public boolean checkDataMonPerConfirm(String employeeId) {
 		String CID = AppContexts.user().companyId();
-		
-		//基準日の会社の締めをすべて取得する
+		// [RQ609]ログイン社員のシステム日時点の処理対象年月を取得する
+		InitDisplayPeriodSwitchSetDto initDisplayPeriodSwitchSetDto = initDisplayPeriodSwitchSetFinder
+				.targetDateFromLogin();
+		List<DateProcessed> listDateProcessed = initDisplayPeriodSwitchSetDto.getListDateProcessed();
+		// 基準日の会社の締めをすべて取得する
 		List<ClosureInfo> listClosure = closureService.getAllClosureInfo();
-		
-		
-		//取得した「締め」からチェック対象を作成する
+
+		// 取得した「締め」からチェック対象を作成する
 		List<CheckTargetItem> listCheckTargetItem = new ArrayList<>();
-		for(ClosureInfo closure : listClosure) {
-			listCheckTargetItem.add(new CheckTargetItem(closure.getClosureId().value,closure.getCurrentMonth()));
+		for (DateProcessed dateProcess : listDateProcessed) {
+			listCheckTargetItem.add(new CheckTargetItem(dateProcess.getClosureID(), dateProcess.getTargetDate()));
 		}
-		//CheckTargetOutPut checkTargetOutPut = new CheckTargetOutPut(listCheckTargetItem);
-		
-		//承認すべき月の実績があるかチェックする
-		boolean result = checkTrackRecord.checkTrackRecord(CID, employeeId, listCheckTargetItem.stream().map(c -> new CheckTargetItemDto(c.getClosureId(), c.getYearMonth())).collect(Collectors.toList()));
+		// CheckTargetOutPut checkTargetOutPut = new
+		// CheckTargetOutPut(listCheckTargetItem);
+
+		// 承認すべき月の実績があるかチェックする
+		boolean result = checkTrackRecord.checkTrackRecord(CID, employeeId, listCheckTargetItem.stream()
+				.map(c -> new CheckTargetItemDto(c.getClosureId(), c.getYearMonth())).collect(Collectors.toList()));
 		return result;
 	}
 }
