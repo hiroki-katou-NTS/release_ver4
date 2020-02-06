b177574f54bdf529281d07723e169025c24164b7
NSVN-DoanDuyHung <hung_dd@nittsusystem-vn.com>
2020-02-06 10:58:00 +0700

    fix bug #109638

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdl/020/a/kdl020.a.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kdl/020/a/kdl020.a.vm.ts
@@ -362,12 +362,16 @@ module nts.uk.at.view.kdl020.a.screenModel {
         daysUsedNo: KnockoutObservable<number> = ko.observable(0);
         usedMinutes: KnockoutObservable<number> = ko.observable(0);
         scheduleRecordAtr: KnockoutObservable<number> = ko.observable(0);
+        workTypeCode: KnockoutObservable<string> = ko.observable("");
+        workTypeName: KnockoutObservable<string> = ko.observable("");
         constructor(data?) {
             if (data) {
                 this.ymd(data.ymd);
                 this.daysUsedNo(data.daysUsedNo);
                 this.usedMinutes(data.usedMinutes);
                 this.scheduleRecordAtr(data.scheduleRecordAtr);
+                this.workTypeCode(data.workTypeCode);
+                this.workTypeName(data.workTypeName);
             }
         }
     }
@@ -467,6 +471,10 @@ module nts.uk.at.view.kdl020.a.screenModel {
         usedMinutes: number;
         /* 予定実績区分*/
         scheduleRecordAtr: number;
+        /* 勤務種類*/
+        workTypeCode: string;
+        
+        workTypeName: string;
     }
 
     export class ListType {
