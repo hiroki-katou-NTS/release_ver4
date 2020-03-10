5f529f7a7707972d48b42fc0d343abd21b2920f2
NSVN-DoanDuyHung <hung_dd@nittsusystem-vn.com>
2020-03-10 17:08:09 +0700

    fix bug #109852 ver 2 ( #109622 UK1)

--- nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kaf/000/b/kaf000.b.vm.ts
+++ nts.uk/uk.at/at.web/nts.uk.at.web/src/main/webapp/view/kaf/000/b/kaf000.b.vm.ts
@@ -157,13 +157,13 @@ module nts.uk.at.view.kaf000.b.viewmodel {
                 self.reasonToApprover(data.authorCmt);
                 let deadlineMsg = data.outputMessageDeadline;
                 if (!nts.uk.text.isNullOrEmpty(deadlineMsg.message)) {
-                    self.reasonOutputMessFull(self.reasonOutputMess + deadlineMsg.message);
+                    self.reasonOutputMessFull(deadlineMsg.message.replace(/\n/ig, '<br/>'));
                 }
                 if(!_.isEmpty(self.reasonOutputMessFull())){
                     self.messFullDisp(true);    
                 }
                 if (!nts.uk.text.isNullOrEmpty(deadlineMsg.deadline)) {
-                    self.reasonOutputMessDealineFull(self.reasonOutputMessDealine + deadlineMsg.deadline);
+                    self.reasonOutputMessDealineFull(deadlineMsg.deadline);
                 }
                 if(!_.isEmpty(self.reasonOutputMessDealineFull())){
                     self.messDealineFullDisp(true);    
