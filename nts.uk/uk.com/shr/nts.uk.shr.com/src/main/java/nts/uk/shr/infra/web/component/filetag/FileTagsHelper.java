4cf00b92ad2bf95b5764ad130b2884d7cac6e747
TruongQuocPhong <phong_tq@nittsusystem-vn.com>
2020-05-06 11:25:55 +0700

    Revert "#fix bug 110306"
    This reverts commit 39239a993adb7481315be62d89a258fc41888729.

--- nts.uk/uk.com/shr/nts.uk.shr.com/src/main/java/nts/uk/shr/infra/web/component/filetag/FileTagsHelper.java
+++ nts.uk/uk.com/shr/nts.uk.shr.com/src/main/java/nts/uk/shr/infra/web/component/filetag/FileTagsHelper.java
@@ -1,12 +1,7 @@
 package nts.uk.shr.infra.web.component.filetag;
 
-import java.io.File;
-import java.util.Date;
-
 import javax.faces.context.FacesContext;
-import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
-import javax.ws.rs.core.Context;
 
 import org.apache.logging.log4j.util.Strings;
 
@@ -14,7 +9,7 @@ import nts.arc.time.GeneralDateTime;
 
 final class FileTagsHelper {
 	
-//	public static String VERSION = GeneralDateTime.now().toString("yyyyMMddHHmmss");
+	public static String VERSION = GeneralDateTime.now().toString("yyyyMMddHHmmss");
 
 	static String buildPath(FacesContext context, String specifiedPath) {
 
@@ -27,23 +22,12 @@ final class FileTagsHelper {
 			filePath = specifiedPath;
 		}
 
-		return appendVersionString(context, filePath);
-	}
-	
-	public static String getVersion(FacesContext context) {
-
-		File file = new File(((ServletContext) context).getRealPath("/view/common/mark.js"));
-
-		if (!file.exists()) {
-			return "xxxxxxxxxxxxxx";
-		}
-
-		return GeneralDateTime.legacyDateTime(new Date(file.lastModified())).toString("yyyyMMddHHmmss");
+		return appendVersionString(filePath);
 	}
 
-	private static String appendVersionString(FacesContext context, String filePath) {
+	private static String appendVersionString(String filePath) {
 
-		return filePath + "?v=" + getVersion(context);
+		return filePath + "?v=" + VERSION;
 	}
 
 	static String buildPathUsingComWeb(FacesContext context, String specifiedPath) {
@@ -57,9 +41,9 @@ final class FileTagsHelper {
 			filePath = specifiedPath;
 		}
 
-		return appendVersionString(context, filePath);
+		return appendVersionString(filePath);
 	}
-	static String buildPathOf(FacesContext context, String appPath ,String specifiedPath) {
+	static String buildPathOf(String appPath ,String specifiedPath) {
 
 		String filePath;
 
@@ -70,6 +54,6 @@ final class FileTagsHelper {
 			filePath = specifiedPath;
 		}
 
-		return appendVersionString(context, filePath);
+		return appendVersionString(filePath);
 	}
 }
