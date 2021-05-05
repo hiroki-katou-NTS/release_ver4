05cf2406d311472d6140139f0ea3d9e4144f3137
NSVN-DoanDuyHung <hung_dd@nittsusystem-vn.com>
2021-05-05 13:29:29 +0700

    fix bug #116222 05/05/2021 ver 2

--- nts.uk/uk.com/pub/nts.uk.pub.spr/src/main/java/nts/uk/pub/spr/SprWebService.java
+++ nts.uk/uk.com/pub/nts.uk.pub.spr/src/main/java/nts/uk/pub/spr/SprWebService.java
@@ -80,7 +80,7 @@ public class SprWebService {
 						for (int i = 0; i < reason.length(); i++) {
 							reasonBytes[i] = (byte)(reason.codePointAt(i));
 						}
-						reasonReal = new String(reasonBytes);
+						reasonReal = new String(reasonBytes, "UTF-8");
 					}
 				}
 			}
